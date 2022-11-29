2e20b56f622d13434b4e23fda117a0e82414e02f
Chisom Ibekwe <lucasbolt700@gmail.com>
2022-11-29 05:49:05 +0100

    task 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-result = (abs(number) % 10)
+result = abs(number) % 10
 if result == 0:
     print("Last digit of {} is {} and is 0".format(number, result))
 else:
