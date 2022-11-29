485889e34a8aa41c1e79a7914223ea7dc1542f07
Chisom Ibekwe <lucasbolt700@gmail.com>
2022-11-29 05:55:11 +0100

    task 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,10 +8,10 @@ else:
     if number > 0:
         if result > 5:
             print("Last digit of {} is {} and is greater than 5"
-            .format(number, result))
+                .format(number, result))
         if result < 6 and result != 0:
             print("Last digit of {} is {} and is less than 6 and not 0"
-            .format(number, result))
+                .format(number, result))
     else:
         print("Last digit of {: d} is {: d} and is less than 6 and not 0"
-        .format(number, -result))
+            .format(number, -result))
