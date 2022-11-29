2c5106d8bdfabb3654b774a74a16fa31e37764b4
Chisom Ibekwe <lucasbolt700@gmail.com>
2022-11-29 04:46:22 +0100

    task 10

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "list.h"
+
+/**
+ * check_cycle - checks if linked list has a loop
+ * @list: a singly-linked list
+ * Return: 0 if not, 1 if true
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *chase, *catch;
+
+	if (list == NULL || list->next == NULL)
+		return (0);
+
+	chase = list->next;
+	catch = list->next->next;
+
+	while (chase && catch && catch->next)
+	{
+		if (chase == catch)
+			return (1);
+		chase = chase->next;
+		catch = catch->next->next;
+	}
+	return (0);
+}
