a74bee13fc9fe7cd292464b3a885b4fb0aafb14b
Chisom Ibekwe <lucasbolt700@gmail.com>
2022-12-01 14:50:15 +0100

    task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     import sys
-    if len(sys.argv) > 1:
-        n = 0
-        count = 1
-        while count < len(sys.argv):
-            n += int(sys.argv[count])
-            count += 1
-        print('{}'.format(n))
-
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+    print("{}".format(total))
