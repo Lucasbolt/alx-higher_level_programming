21e765e50ccff358a12f06fc1c717bfe1704a87d
Chisom Ibekwe <lucasbolt700@gmail.com>
2022-12-05 10:45:05 +0100

    task 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,7 +3,7 @@ def replace_in_list(my_list, idx, element):
     a = len(my_list)
     if idx < 0:
         return my_list
-    if idx > a:
+    if idx > (a - 1):
         return my_list
     my_list[idx] = element
     return my_list
