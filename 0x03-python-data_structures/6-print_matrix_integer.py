fc76cb34eaf88cf81992e6fb76f1ef9de878425c
Chisom Ibekwe <lucasbolt700@gmail.com>
2022-12-05 10:55:34 +0100

    task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for a in matrix:
         for b in a:
             if b == a[-1]:
-                print('{}'.format(b), end='')
+                print('{:d}'.format(b), end='')
             else:
-                print('{}'.format(b), end=' ')
+                print('{:d}'.format(b), end=' ')
         print('')
