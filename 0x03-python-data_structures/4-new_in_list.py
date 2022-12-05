b823b99f5d55aa341f9eb0b00783dcf4546de0b0
Chisom Ibekwe <lucasbolt700@gmail.com>
2022-12-05 10:53:55 +0100

    task 4

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     count = len(my_list)
-    if idx > count:
+    if idx > (count - 1):
         return my_list
     if idx < 0:
         return my_list
