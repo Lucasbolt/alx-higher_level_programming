8be17c502caa2e41a54f72bedf8010aa5f709557
Chisom Ibekwe <lucasbolt700@gmail.com>
2022-12-09 02:48:29 +0100

    fixed it!

--- 0x04-python-more_data_structures/test.py
+++ 0x04-python-more_data_structures/test.py
@@ -1,12 +1,38 @@
 #!/usr/bin/python3
-square_matrix_simple = __import__('0-square_matrix_simple').square_matrix_simple
+import sys
+def myconv(number = ""):
+    if len(number) == 0 or number == None:
+        return 0
 
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
+    conv = {
+            'I':1,
+            'V':5,
+            'X':10,
+            'L':50,
+            'C':100,
+            'D':500,
+            'M':1000
+            }
 
-new_matrix = square_matrix_simple(matrix)
-print(new_matrix)
-print(matrix)
+    result = 0
+    digits = []
+    for i in number:
+        if i in conv.keys():
+            digits.append(conv[i])
+        else:
+            print("Unrecognized letter")
+            sys.exit()
+    if digits[-1] == max(digits) and  digits[0] != digits[-1]:
+        result = digits[-1]
+        for i in range(len(digits) - 1):
+            result-= digits[i]
+        return result
+    else:
+        result = sum(digits)
+        return result
+if __name__ == '__main__':
+    print(myconv('II'))
+    print(myconv('IVQ'))
+    print(myconv('DCCVII'))
+    print(myconv('LXXXVII'))
+    print(myconv('VII'))
