92db8d7e18d0da63d2bf0b5fe4680df4ed20c631
Chisom Ibekwe <lucasbolt700@gmail.com>
2022-12-21 03:58:01 +0100

    task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,27 @@
+#!usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0):
+        """
+        initialize a new square.
+
+        Args:
+            size (int): the size of the new square.
+
+        Raises:
+            TypeError: if size is not an integer.
+            ValueError: if size is less than 0.
+        """
+        if size is not int:
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be > 0')
+        self.__size = size
+
+    def area(self):
+        """computes aras of Square"""
+        return self.__size * self.__size
