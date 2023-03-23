0d255cb6f0780b8b1862cf4298fea0b6e2ac7ac5
Chisom Ibekwe <lucasbolt700@gmail.com>
2023-03-23 16:37:02 +0100

    fixed task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 """
 Lists all states from the database hbtn_0e_0_usa.
-Usage: ./0-select_states.py <mysql username> \
-                             <mysql password> \
-                              <database name>
 """
 import sys
 import MySQLdb
@@ -19,3 +16,6 @@ if __name__ == "__main__":
     c = db.cursor()
     c.execute("SELECT * FROM `states`")
     [print(state) for state in c.fetchall()]
+    c.close()
+    db.close()
+    
